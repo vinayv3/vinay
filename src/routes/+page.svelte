1c7e221953f23479207b7cc53bdec05c03864bc2
Sanjay M <arjunsanjay0@gmail.com>
2023-02-03 15:55:48 +0530

    added case studies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="m-10 font-Inter">
+<div class="container mx-auto my-10 font-Inter">
 	<nav class="flex justify-between uppercase">
 		<a class="underline" href="">Vinay Kumar</a>
 
@@ -9,15 +9,15 @@
 		</div>
 	</nav>
 
-	<main class="flex items-center justify-center pt-60">
-		<div class="m-10 text-center text-xl font-medium">
-			<p class="font-medium mb-8">
+	<main class="flex items-center justify-center pt-40">
+		<div class="m-10 text-center text-xl font-semibold">
+			<p class="font-bold mb-8">
 				Hi, I'm <span
-					class="text-transparent bg-clip-text bg-gradient-to-r from-vinay to-kumar text-6xl font-bold "
+					class="text-transparent bg-clip-text bg-gradient-to-r from-vinay to-kumar text-6xl"
 					>Vinay Kumar</span
 				>
 			</p>
-			<p>UI/UX Designer based in Bangalore. Specialized in</p>
+			<p class="">UI/UX Designer based in Bangalore. Specialized in</p>
 			<p>creating better design experiences for people by</p>
 			<p>
 				using skills in <span
@@ -27,4 +27,46 @@
 			</p>
 		</div>
 	</main>
+
+	<div class="flex items-center justify-between container mx-auto">
+		<div>
+			<p class="font-bold text-2xl mb-4">TechFino Partner Application</p>
+			<p>
+				Designed the partner application for the techfino company to help their partners maximize
+				their business potential. Partnering with Techfino, a fintech startup company, enables their
+				partners to utilize the Partner Application, simplifying the process of leading customers to
+				services.
+			</p>
+		</div>
+
+		<img class="justify-self-end" src="partner.png" alt="" />
+	</div>
+
+	<div class="flex items-center justify-between container mx-auto">
+		<img class="justify-self-end" src="customer.png" alt="" />
+
+		<div>
+			<p class="font-bold text-2xl mb-4">TechFino Customer Application</p>
+			<p>
+				Designed the partner application for the techfino company to help their partners maximize
+				their business potential. Partnering with Techfino, a fintech startup company, enables their
+				partners to utilize the Partner Application, simplifying the process of leading customers to
+				services.
+			</p>
+		</div>
+	</div>
+
+	<div class="flex items-center justify-between container mx-auto">
+		<div>
+			<p class="font-bold text-2xl mb-4">TechFino HRMS Application</p>
+			<p>
+				Designed an HRMS application for Techfino company to streamline their HR processes.. We
+				custom-designed an HRMS application for Techfino Capital so that it could facilitate HRs to
+				gain insight into the details, and progress of each employee, as well as provide them with
+				the required records.
+			</p>
+		</div>
+
+		<img class="justify-self-end" src="hrms.png" alt="" />
+	</div>
 </div>
